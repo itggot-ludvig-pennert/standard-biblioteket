183ad93b93421424617b4e5f036ac309646cda17
itggot-ludvig-pennert <38528579+itggot-ludvig-pennert@users.noreply.github.com>
2018-04-19 14:58:26 +0200

    bugfix between

--- lib/between.rb
+++ lib/between.rb
@@ -1,6 +1,6 @@
 def between(term1, term2, term3)
     answer = false
-    if term2 < term1 && term1 < term3
+    if term2 <= term1 && term1 <= term3
         answer = true
     end
     return answer
