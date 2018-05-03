d623424ae4f2b672651878b349fe813231315dd0
itggot-ludvig-pennert <38528579+itggot-ludvig-pennert@users.noreply.github.com>
2018-05-03 15:12:57 +0200

    la till last_of

---
+++ lib/last_of.rb
@@ -0,0 +1,12 @@
+# Public: Takes an array and returns the last element
+# 
+# array - The array
+#
+# Examples
+#   first_of([5, 2, 76, 3])
+#   # => "3"
+#
+# Returns the last element in the array
+def last_of(array)
+    return array[array.length - 1]
+end
