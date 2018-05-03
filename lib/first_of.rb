ad4976cd606cbf3cd3e8d03d48f626cae859fa7b
itggot-ludvig-pennert <38528579+itggot-ludvig-pennert@users.noreply.github.com>
2018-05-03 15:13:06 +0200

    la till first_of

---
+++ lib/first_of.rb
@@ -0,0 +1,12 @@
+# Public: Takes an array and returns the first element
+# 
+# array - The array
+#
+# Examples
+#   first_of([5, 2, 76, 3])
+#   # => "5"
+#
+# Returns the first element in the array
+def first_of(array)
+    return array[0]
+end
